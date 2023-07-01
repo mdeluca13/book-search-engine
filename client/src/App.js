f5a1495183e6e9417840d40d2a6668e4db8cd2fd
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-07-01 10:12:25 -0400

    converted client app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
 
+// Adding graphql endpoint
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Request middleware for auth
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+// Adding client to run authLink middleware before graphql api request
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+// App function to determine what to display
 function App() {
   return (
     <Router>
