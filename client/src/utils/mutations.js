87574fa67847cd49e8d26a4de6f3afdf28be7feb
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-07-02 11:15:42 -0400

    added client mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,3 +24,40 @@ export const ADD_USER = gql`
     }
 `;
 
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: savedBook) {
+        saveBook(input: $input) {
+            _id
+            username
+            email
+            bookCount
+            saveBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        removeBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            saveBooks {
+                bookId
+                title
+                authors
+                description
+                image
+                link
+            }
+        }
+    }
+`;
