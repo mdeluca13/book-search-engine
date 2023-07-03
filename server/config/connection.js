6e3a66256d042a75bc13b0a382ae795f6ed75b58
mdeluca13 <123423832+mdeluca13@users.noreply.github.com>
2023-07-02 21:09:39 -0400

    heroku push

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 // Importing necessary files
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/booksDB', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://174.112.15.206/32/booksDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
